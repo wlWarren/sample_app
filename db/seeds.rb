c6bedd5a026c9505365bf868705606d62e63243a
bao_since95 <457771526@qq.com>
2018-01-27 20:14:50 +0800

    AA

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "bhbbhb",
+User.create!(name:  "bhb",
              email: "bhb@ucas.cn",
              password:              "bhbbhb",
              password_confirmation: "bhbbhb",
@@ -15,7 +15,7 @@ User.create!(name:  "bhbbhb",
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@rails.org"
+  email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
                email: email,
